33b37f97e4d475b7e28cbc1f3fca578254d9e8f4
takane20 <eloisemable17@gmail.com>
2026-07-10 15:30:39 +0100

    Update ECOPARVIS.py

--- ECOPARVIS.py
+++ ECOPARVIS.py
@@ -18,6 +18,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import cm
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
 from reportlab.lib.enums import TA_CENTER, TA_LEFT
+
 # Mise à jour
 
 # ─── Configuration ───────────────────────────────────────────────────────────
